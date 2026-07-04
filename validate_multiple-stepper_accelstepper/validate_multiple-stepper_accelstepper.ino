fd4c2133b9e4c59bf35de67ddfebf2af9b341c1b
Dolne <Portal2fun@gmail.com>
2026-07-04 17:14:42 +0800

    Validated 4 steppers at once when using lower max speed & acceleration

--- validate_multiple-stepper_accelstepper/validate_multiple-stepper_accelstepper.ino
+++ validate_multiple-stepper_accelstepper/validate_multiple-stepper_accelstepper.ino
@@ -20,10 +20,17 @@ The function runSpeed() determines when a step is to be taken. runSpeed() subtra
 // $Id: Random.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
 
 // defines pins
-#define A_STEP_PIN 2
-#define A_DIR_PIN 5 
-#define B_STEP_PIN 2
-#define B_DIR_PIN 5 
+#define A_STEP_PIN 26
+#define A_DIR_PIN 12
+
+#define B_STEP_PIN 27
+#define B_DIR_PIN 13
+
+#define C_STEP_PIN 32
+#define C_DIR_PIN 14
+
+#define D_STEP_PIN 33
+#define D_DIR_PIN 15
 
 
 //Leave the enable pin unconnected / grounded. If you connect it to 3V3, it will stop working
@@ -31,26 +38,57 @@ The function runSpeed() determines when a step is to be taken. runSpeed() subtra
 //1.8 degrees per step & 8 microsteps per step means 1600 ((360/1.8)*8=1600) steps to turn 360 degrees
 
 #include <AccelStepper.h>
-#include <MultiStepper.h>
+/*#include <MultiStepper.h>
+https://www.airspayce.com/mikem/arduino/AccelStepper/classMultiStepper.html#details
+don't need use multistepper since multistepper is more to cause all the steppers to move to selected positions at such a (constant) speed that they all arrive at their target position at the same time. This can be used to support devices with multiple steppers on say multiple axes to cause linear diagonal motion. Suitable for use with X-Y plotters, flatbeds, 3D printers etc to get linear straight line movement between arbitrary 2d (or 3d or ...) positions.*/
 
 
-// Define a stepper and the pins it will use
-AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN); // With args, Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+// Define steppers and the pins it will use
+AccelStepper stepperA(AccelStepper::DRIVER, A_STEP_PIN, A_DIR_PIN);
+AccelStepper stepperB(AccelStepper::DRIVER, B_STEP_PIN, B_DIR_PIN);
+AccelStepper stepperC(AccelStepper::DRIVER, C_STEP_PIN, C_DIR_PIN);
+AccelStepper stepperD(AccelStepper::DRIVER, D_STEP_PIN, D_DIR_PIN);
 
 void setup()
 {  
   // Change these to suit your stepper if you want
-  stepper.setMaxSpeed(12000); //Approximate based on basic testing of validate_stepper script
-  stepper.setAcceleration(800); //Expensive call, calcs a sqrt. 
-  stepper.moveTo(160000); //100 full rotations
+  stepperA.setMaxSpeed(3000); //Approximate based on basic testing of validate_stepper script. Originally 12000, quartered to reduce current draw
+  stepperB.setMaxSpeed(3000);
+  stepperC.setMaxSpeed(3000);
+  stepperD.setMaxSpeed(3000);
+
+  stepperA.setAcceleration(200); //Expensive call, calcs a sqrt. Originally 800, quartered to reduce current draw
+  stepperB.setAcceleration(200);
+  stepperC.setAcceleration(200);
+  stepperD.setAcceleration(200);
+
+  stepperA.moveTo(160000); //100 full rotations
+  stepperB.moveTo(160000); //100 full rotations
+  stepperC.moveTo(160000);
+  stepperD.moveTo(160000);
+
   //setCurrentPosition((long) position) this function does homing
 }
 
 void loop()
 {
   // If at the end of travel go to the other end
-  if (stepper.distanceToGo() == 0)
-    stepper.moveTo(-stepper.currentPosition());
+  if (stepperA.distanceToGo() == 0) {
+    stepperA.moveTo(-stepperA.currentPosition());
+  }
+  if (stepperB.distanceToGo() == 0) {
+    stepperB.moveTo(-stepperB.currentPosition());
+  }
+  if (stepperC.distanceToGo() == 0) {
+    stepperC.moveTo(-stepperC.currentPosition());
+  }
+  if (stepperD.distanceToGo() == 0) {
+    stepperD.moveTo(-stepperD.currentPosition());
+  }
 
-  stepper.run();
+  
+  stepperA.run();
+  stepperB.run();
+  stepperC.run();
+  stepperD.run();
 }
