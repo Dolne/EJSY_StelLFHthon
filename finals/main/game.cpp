53b2829d3628805ac495c35f6f9bd05ef4b1f3dd
lam eu ler <27113373+lameuler@users.noreply.github.com>
2026-07-31 16:24:16 +0800

    add comments for game round code

--- finals/main/game.cpp
+++ finals/main/game.cpp
@@ -2,93 +2,135 @@
 
 /**
  * takes a random sample of size `enabled` and sets to 1, while all others set to 0.
+ * this is used to randomly enable a specified number of features.
  */
-void enableFeatures(uint8_t* features, uint8_t featuresCount, uint8_t enabled) {
+void enableFeatures(uint8_t *features, uint8_t featuresCount, uint8_t enabled)
+{
     uint8_t done = 0;
-    for (int i = 0; i < featuresCount; i++) {
-        if (random(featuresCount - i) < (enabled - done)) {
+    for (int i = 0; i < featuresCount; i++)
+    {
+        if (random(featuresCount - i) < (enabled - done))
+        {
             features[i] = 1;
             done++;
-        } else {
+        }
+        else
+        {
             features[i] = 0;
         }
     }
 }
 
-bool hasFeatures(uint8_t* features, uint8_t featuresCount) {
-    for (int i = 0; i < featuresCount; i++) {
-        if (features[i] != 0) {
+bool hasFeatures(uint8_t *features, uint8_t featuresCount)
+{
+    for (int i = 0; i < featuresCount; i++)
+    {
+        if (features[i] != 0)
+        {
             return true;
         }
     }
     return false;
 }
 
-bool generateSlots(uint8_t slots[4], uint8_t slotCount, uint8_t odd1OutSlot, uint8_t enabled, uint8_t* features, uint8_t featuresCount, uint8_t offset)
+/**
+ * generate the index to display/play for each slot.
+ */
+bool generateSlots(uint8_t slots[4], uint8_t slotCount, uint8_t odd1OutSlot, uint8_t enabled, uint8_t *features, uint8_t featuresCount, uint8_t offset)
 {
-    if (enabled == 0) {
-        // need to do anything? eg set slots to all NONE
+    // stimuli is not enabled (off)
+    if (enabled == 0)
+    {
         return false;
-    } else if (enabled > 1) {
+    }
+    // stimuli is enabled with the number of features specified (1 diff, 2 diff, etc)
+    else if (enabled > 1)
+    {
+        // need to randomly enable (n) features (ie choose which features will differ)
         enableFeatures(features, featuresCount, enabled - 1);
     }
-    if (!hasFeatures(features, featuresCount)) {
+    // otherwise it means stimuli is enabled with manually selected features (manual)
+    // need to check if any features have actually been enabled manually
+    else if (!hasFeatures(features, featuresCount))
+    {
         return false;
     }
-    // generate 2 values: the odd-1-out, and the value for the other slots
-    uint8_t odd1Out = random(1 << featuresCount); // 2^n
-    uint8_t other = odd1Out;
+
+    // randomly generate the odd-1-out option
+    uint8_t odd1Out = random(1 << featuresCount); // 0 <= odd1Out < 2^featuresCount
+
     // the other value is the odd-1-out but with all the enabled features toggled
-    for (int i = 0; i < featuresCount; i++) {
-        if (features[i] == 1) {
+    uint8_t other = odd1Out;
+    for (int i = 0; i < featuresCount; i++)
+    {
+        if (features[i] == 1)
+        {
             other ^= (1 << i); // flip the bit corresponding to the feature
         }
     }
-    for (int i = 0; i < MAX_SLOTS; i++) {
-        if (i < slotCount) {
+
+    // populate the slots array with the index to show for each slot
+    for (int i = 0; i < MAX_SLOTS; i++)
+    {
+        if (i < slotCount)
+        {
             // add to the index value generated as 0 represents null
             // for visual 1 is also a special display slot
-            if (i == odd1OutSlot) {
+            if (i == odd1OutSlot)
+            {
                 slots[i] = odd1Out + offset;
-            } else {
+            }
+            else
+            {
                 slots[i] = other + offset;
             }
-        } else {
+        }
+        // can have unused slots based on number of slots chosen by gamemaster
+        else
+        {
+            // 0 represents null (ie nothing will be displayed or played)
             slots[i] = 0;
         }
     }
     return true;
 }
 
-GameRound::GameRound(GameOptions opts):
-    slotsCount(opts.slotsCount + MIN_SLOTS),
-    inputMode(opts.inputMode),
-    odd1OutSlot(random(slotsCount)),
-    hasVisual(opts.visual > 0),
-    hasAudio(opts.audio > 0 && opts.inputMode == 0), // audio can only be active if input mode is "scanning"
-    hasTactile(opts.tactile > 0 && opts.inputMode == 1) // tactile can only be active if input mode is "select"
+GameRound::GameRound(GameOptions opts) : slotsCount(opts.slotsCount + MIN_SLOTS),
+                                         inputMode(opts.inputMode),
+                                         odd1OutSlot(random(slotsCount)), // randomly select the slot to use as the odd one out
+                                         hasVisual(opts.visual > 0),
+                                         hasAudio(opts.audio > 0 && opts.inputMode == 0),    // audio can only be active if input mode is "scanning"
+                                         hasTactile(opts.tactile > 0 && opts.inputMode == 1) // tactile can only be active if input mode is "select"
 {
-    if (hasVisual) {
+    // generate the options to present to the user in that round based on the game options
+
+    if (hasVisual)
+    {
         hasVisual = generateSlots(visual, slotsCount, odd1OutSlot, opts.visual, opts.visualOptions, VISUAL_FEATS_COUNT, VISUAL_VALUE_OFFSET);
     }
 
-    if (hasAudio) {
+    if (hasAudio)
+    {
         hasAudio = generateSlots(audio, slotsCount, odd1OutSlot, opts.audio, opts.audioOptions, AUDIO_FEATS_COUNT, AUDIO_VALUE_OFFSET);
     }
 
+    // if there are no features enabled at all, it means it will be in the "null" gameplay mode
     isNullRound = !hasVisual && !hasAudio && !hasTactile;
 }
 
-
-void printSlots(uint8_t slots[4], uint8_t digits, uint8_t offset) {
+void printSlots(uint8_t slots[4], uint8_t digits, uint8_t offset)
+{
     Serial.print(" [");
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         Serial.print(slots[i]);
         Serial.print(" ");
-        if (slots[i] >= offset) {
+        if (slots[i] >= offset)
+        {
             Serial.print("(");
             uint8_t val = slots[i] - offset;
-            for (int j = digits - 1; j >= 0; j--) {
+            for (int j = digits - 1; j >= 0; j--)
+            {
                 Serial.print(bitRead(val, j));
             }
             Serial.print(") ");
@@ -97,14 +139,17 @@ void printSlots(uint8_t slots[4], uint8_t digits, uint8_t offset) {
     Serial.println("]");
 }
 
-void printGameRound(GameRound* gameRound)
+void printGameRound(GameRound *gameRound)
 {
     Serial.print("slots: ");
     Serial.println(gameRound->slotsCount);
     Serial.print("odd 1 out: ");
-    if (gameRound->isNullRound) {
+    if (gameRound->isNullRound)
+    {
         Serial.println("NULL");
-    } else {
+    }
+    else
+    {
         Serial.println(gameRound->odd1OutSlot);
     }
 
@@ -123,22 +168,21 @@ void printGameRound(GameRound* gameRound)
 GameHardware::GameHardware(
     LCD &lcd,
     const ButtonGroup &inputButtons, const Button &debugButton,
-    StepperGroup& steppers,
-    AudioPlayer& audio,
-    Adafruit_NeoPixel& scanningStrip, Adafruit_NeoPixel& feedbackStrip,
-    OutputController& vibration, const int* v1, int l1, const int* v2, int l2
-):
-    lcd(lcd),
-    inputButtons(inputButtons),
-    debugButton(debugButton),
-    steppers(steppers),
-    audio(audio),
-    scanningStrip(scanningStrip),
-    feedbackStrip(feedbackStrip),
-    vibration(vibration),
-    vibrationSeqSuccess(v1),
-    vibrationSeqSuccessLen(l1),
-    vibrationSeqFail(v2),
-    vibrationSeqFailLen(l2)
+    StepperGroup &steppers,
+    AudioPlayer &audio,
+    Adafruit_NeoPixel &scanningStrip, Adafruit_NeoPixel &feedbackStrip,
+    OutputController &vibration,
+    const int *v1, int l1, const int *v2, int l2) : lcd(lcd),
+                                                    inputButtons(inputButtons),
+                                                    debugButton(debugButton),
+                                                    steppers(steppers),
+                                                    audio(audio),
+                                                    scanningStrip(scanningStrip),
+                                                    feedbackStrip(feedbackStrip),
+                                                    vibration(vibration),
+                                                    vibrationSeqSuccess(v1),
+                                                    vibrationSeqSuccessLen(l1),
+                                                    vibrationSeqFail(v2),
+                                                    vibrationSeqFailLen(l2)
 {
 }
--- finals/main/game.h
+++ finals/main/game.h
@@ -8,58 +8,59 @@
 #include "button.h"
 #include "state.h"
 
-inline const char* OPTS_ON_OFF[] = { "off", "on" };
-// this is meant to accomodate up to the stimuli with the most features
-// TODO [ux] does it make sense for manual to come first? 
-inline const char* OPTS_DIFFS[] = { "off", "manual", "1 diff", "2 diff", "3 diff", "4 diff" };
-inline const char* OPTS_VOLUME[] = { "low", "medium", "high" };
-inline const uint8_t VOLUME_VALUES[] = { 10, 20, 30 };
-inline const char* OPTS_SCAN_SPEED[] = { "2s", "5s", "10s" };
-inline const int SCAN_SPEED_VALUES[] = { 2000, 5000, 10000 };
+// option names to show in menu
 
-inline const char* OPTS_SLOTS[] = { "3", "4" };
+inline const char *OPTS_ON_OFF[] = {"off", "on"};
+// this is meant to accomodate up to the stimuli with the most features
+inline const char *OPTS_DIFFS[] = {"off", "manual", "1 diff", "2 diff", "3 diff", "4 diff"};
+
+inline const char *OPTS_VOLUME[] = {"low", "medium", "high"};
+inline const uint8_t VOLUME_VALUES[] = {10, 20, 30};
+
+inline const char *OPTS_SCAN_SPEED[] = {"2s", "5s", "10s"};
+inline const int SCAN_SPEED_VALUES[] = {2000, 5000, 10000};
+
+inline const char *OPTS_SLOTS[] = {"3", "4"};
 inline const int MIN_SLOTS = 3;
 
-inline const char* OPTS_ROUNDS[] = { "1", "2", "3", "4" };
+inline const char *OPTS_ROUNDS[] = {"1", "2", "3", "4"};
 inline const int MIN_ROUNDS = 1;
 
-inline const char* OPTS_INPUT_MODE[] = { "scanning", "select" };
+inline const char *OPTS_INPUT_MODE[] = {"scanning", "select"};
 inline const int INPUT_MODE_SCANNING = 0;
 inline const int INPUT_MODE_SELECT = 1;
 
-inline const int VISUAL_FEATS_COUNT = 3;  // shape colour size
-inline const int VISUAL_VALUE_OFFSET = 2;
+inline const int VISUAL_FEATS_COUNT = 3; // size colour shape
+inline const int VISUAL_VALUE_OFFSET = 2; // null and display/reserved
 inline const int VISUAL_TOTAL_VALUES = (1 << VISUAL_FEATS_COUNT) + VISUAL_VALUE_OFFSET;
-inline const int AUDIO_FEATS_COUNT = 4;   // loudness pitch timbre L/R
-inline const int AUDIO_VALUE_OFFSET = 1;
+inline const int AUDIO_FEATS_COUNT = 4; // loudness pitch timbre L/R
+inline const int AUDIO_VALUE_OFFSET = 1; // null
 
 inline const int MAX_SLOTS = 4;
 
 class GameHardware
 {
 public:
-    GameHardware(LCD& lcd, const ButtonGroup& inputButtons, const Button& debugButton, StepperGroup& steppers, AudioPlayer& audio, Adafruit_NeoPixel& scanningStrip, Adafruit_NeoPixel& feedbackStrip, OutputController& vibration, const int* v1, int l1, const int* v2, int l2);
-    LCD& lcd;
-    const ButtonGroup& inputButtons;
-    const Button& debugButton;
-    StepperGroup& steppers;
-    AudioPlayer& audio;
-    Adafruit_NeoPixel& scanningStrip;
-    Adafruit_NeoPixel& feedbackStrip;
-    OutputController& vibration;
-    const int* vibrationSeqSuccess;
+    GameHardware(LCD &lcd, const ButtonGroup &inputButtons, const Button &debugButton, StepperGroup &steppers, AudioPlayer &audio, Adafruit_NeoPixel &scanningStrip, Adafruit_NeoPixel &feedbackStrip, OutputController &vibration, const int *v1, int l1, const int *v2, int l2);
+    LCD &lcd;
+    const ButtonGroup &inputButtons;
+    const Button &debugButton;
+    StepperGroup &steppers;
+    AudioPlayer &audio;
+    Adafruit_NeoPixel &scanningStrip;
+    Adafruit_NeoPixel &feedbackStrip;
+    OutputController &vibration;
+    const int *vibrationSeqSuccess;
     int vibrationSeqSuccessLen;
-    const int* vibrationSeqFail;
+    const int *vibrationSeqFail;
     int vibrationSeqFailLen;
 };
-
-// for custom game mode create another options class and add a constructor to GameRound
 
 class GameOptions
 {
 public:
     bool enabled = false;
-    
+
     uint8_t rounds = 0;
     uint8_t inputMode = 0;
     uint8_t scanSpeed = 0;
@@ -68,34 +69,71 @@ public:
     uint8_t volume = 2;
 
     uint8_t visual = 0;
-    uint8_t visualOptions[VISUAL_FEATS_COUNT] = { 0 };
+    uint8_t visualOptions[VISUAL_FEATS_COUNT] = {0};
     uint8_t audio = 0;
-    uint8_t audioOptions[AUDIO_FEATS_COUNT] = { 0 };
+    uint8_t audioOptions[AUDIO_FEATS_COUNT] = {0};
     uint8_t tactile = 0;
 };
 
 class GameRound
 {
 public:
+    /**
+     * constructor which receives the game options to generate a game round
+     */
     GameRound(GameOptions opts);
-    uint8_t slotsCount; // 3 or 4
+    /**
+     * the number of slots used for the round. should be 3 or 4.
+     */
+    uint8_t slotsCount;
+    /**
+     * the input mode configured. either 0 (`INPUT_MODE_SCANNING`) or 1 (`INPUT_MODE_SELECT`).
+     */
     uint8_t inputMode;
     /**
-     * The actual/correct odd one slot. 0 <= odd1OutSlot < slotsCount.
+     * the actual/correct odd one slot. 0 <= odd1OutSlot < slotsCount.
      */
     uint8_t odd1OutSlot;
     /**
-     * The user's answer. 0xFF if no answer given.
+     * the user's answer. 0xFF if no answer given.
      */
     uint8_t answer = 0xFF;
+
+    /**
+     * whether there is any visual stimuli for the round
+     * if `false`, `visual` will be all 0
+     */
     bool hasVisual = false;
-    uint8_t visual[MAX_SLOTS] = { 0 };
+    /**
+     * the index of the option to show for each slot
+     * any unused slots will have a value of 0
+     */
+    uint8_t visual[MAX_SLOTS] = {0};
+
+    /**
+     * whether there is any audio stimuli for the round
+     * if `false`, `audio` will be all 0
+     */
     bool hasAudio = false;
-    uint8_t audio[MAX_SLOTS] = { 0 };
+    /**
+     * the index of the option to play for each slot
+     * any unused slot will have a value of 0
+     */
+    uint8_t audio[MAX_SLOTS] = {0};
+
+    /**
+     * whether there is any tactile stimuli for the round
+     */
     bool hasTactile = false;
+    /**
+     * whether this is a null round, meaning there is no stimuli active in the round
+     */
     bool isNullRound = false;
 };
 
-void printGameRound(GameRound* gameRound);
+/**
+ * print out the details of a game round for debugging
+ */
+void printGameRound(GameRound *gameRound);
 
 #endif
